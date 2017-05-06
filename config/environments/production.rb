45964794ee14ecba846405dcb08db7f485b4ccfb
miskaes <dominika.szlendak@gmail.com>
2017-05-06 22:49:12 +0200

    update git ignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,11 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    s3_region: ENV['AWS_REGION'],
     s3_credentials: {
       bucket: ENV['AWS_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION']
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
